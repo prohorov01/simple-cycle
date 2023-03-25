d80c0b1e45221ceffcc3bc354b8d1c9eb97f3865
prohorov01 <alexprohorov01@gmail.com>
2023-03-25 19:23:27 +0200

    update no work

---
+++ main.js
@@ -0,0 +1,69 @@
+// for (let i = 10; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// for (let a = 7; a <= 70; a += 7) {
+//   console.log(a);
+// }
+
+// for (let a = 10; a <= 20; a++) {
+//   console.log(a);
+//   2 + a ;
+//   console.log("//", a);
+
+//     console.log("////", a);
+// }
+
+// let n = 0;
+// let x = 0;
+
+// while (n < 15) {
+//   n++;
+//   x += n;
+//   console.log("//", n);
+
+//   console.log("////", x);
+// }
+
+// let n = 0;
+// let x = 30;
+
+// while (n < 25) {
+//   n++;
+//   x += 2;
+//   console.log("//", n);
+
+//   console.log("////", x);
+// }
+
+// let n = 0;
+// let x = 100;
+
+// while (n < 33) {
+//   n++;
+//   x += 3;
+//   console.log("/n/", n);
+
+//   console.log("//x//", x);
+// }
+
+// let n = 14;
+// let x = 35;
+
+// while (n < 35) {
+//   n++;
+//   x *= n;
+//   console.log("/n/", n);
+
+//   console.log("//x//", x);
+// }
+let n = 0;
+let x = 12;
+
+while (n < 7) {
+  n++;
+  x /= n * 12;
+  console.log("/n/", n);
+
+  console.log("//x//", x);
+}
